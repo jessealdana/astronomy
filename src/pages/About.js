d2470dbc7da3942ba50300dcb4241144d655bf74
Jesse Aldana <jesse.aldana@gmail.com>
2020-11-18 15:50:12 -0800

    add and subtract modules

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,10 @@ import titleBackground from "../components/images/title.png";
 import pageOne from "../components/images/pageOne.png";
 import pageTwo from "../components/images/pageTwo.png";
 import pageThree from "../components/images/pageThree.png";
-
+import pageFour from "../components/images/pageFour.png";
+import pageFive from "../components/images/pageFive.png";
+import pageSix from "../components/images/pageSix.png";
+import pageSeven from "../components/images/mp8.png";
 
 function MyBook(props) {
   return (
@@ -19,18 +22,38 @@ function MyBook(props) {
             </div>
           </div>
           <div className="demoPage">
-          <div className="pageContent">
+            <div className="pageContent">
               <div className="page-image" style={{backgroundImage: `url(${pageOne})`, width: "1000px", height: "1000px"}}>Page 2</div>
             </div>
           </div>
           <div className="demoPage">
-          <div className="pageContent">
+            <div className="pageContent">
               <div className="page-image" style={{backgroundImage: `url(${pageTwo})`, width: "1000px", height: "1000px"}}>Page 3</div>
             </div>
           </div>
           <div className="demoPage">
-          <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageThree})`, width: "1000px", height: "1000px"}}>Page 3</div>
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageThree})`, width: "1000px", height: "1000px"}}>Page 4</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageFour})`, width: "1000px", height: "1000px"}}>Page 5</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageFive})`, width: "1000px", height: "1000px"}}>Page 6</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageSix})`, width: "1000px", height: "1000px"}}>Page 7</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageSeven})`, width: "1000px", height: "1000px"}}>Page 8</div>
             </div>
           </div>
         </HTMLFlipBook>
--- src/pages/ModThree.js
+++ src/pages/ModThree.js
@@ -1,16 +1,52 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import "../styles.css";
+import HTMLFlipBook from "react-pageflip";
+import titleBackground from "../components/images/sp1.png";
+import pageOne from "../components/images/sp2.png";
+import pageTwo from "../components/images/sp3.png";
+import pageThree from "../components/images/ap4.png";
+import pageFour from "../components/images/ap5.png";
+import pageFive from "../components/images/ap6.png";
 
-export default function ModThree() {
+function ModThree(props) {
   return (
-    <div className="about">
+    <div className="myBook">
       <Navigation />
-      <div className="container">
-        <div className="row" style={{marginTop: "20%"}}>
-
-        </div>
-      </div>
+        <HTMLFlipBook width={1000} height={800}>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${titleBackground})`, width: "1000px", height: "1000px"}}>Page 1</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageOne})`, width: "1000px", height: "1000px"}}>Page 2</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageTwo})`, width: "1000px", height: "1000px"}}>Page 3</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageThree})`, width: "1000px", height: "1000px"}}>Page 4</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageFour})`, width: "1000px", height: "1000px"}}>Page 5</div>
+            </div>
+          </div>
+          <div className="demoPage">
+            <div className="pageContent">
+              <div className="page-image" style={{backgroundImage: `url(${pageFive})`, width: "1000px", height: "1000px"}}>Page 6</div>
+            </div>
+          </div>
+        </HTMLFlipBook>
     </div>
   );
 }
+
+export default ModThree;
