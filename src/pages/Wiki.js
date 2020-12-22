5460c58f2a38a79445faae9ccef357f81c5fc1b3
Jesse Aldana <jesse.aldana@gmail.com>
2020-12-22 10:24:11 -0800

    fixed relative postions of codices on wiki shelves

--- src/pages/Wiki.js
+++ src/pages/Wiki.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import "../styles.css";
+import home from "../components/images/ceAkatl.png";
 import collab from "../components/images/collab.png";
 import sample from "../components/images/events.png"
 
@@ -8,10 +9,12 @@ import sample from "../components/images/events.png"
 export default function Wiki() {
   return (
     <>
-    <Navigation />
     <div className="wiki">
       <div className="container">
         <div className="main">
+            <a href="/">
+                <img id="homePic" src={home} alt="Home" />
+            </a>
             <a href="/sample">
                 <img id="samplePic" src={sample} alt="Events" />
             </a>
