151a4b5ac8cbbb7c85ea13bcdbd0bf7263b5efd0
Jesse Aldana <jesse.aldana@gmail.com>
2020-12-28 15:58:07 -0800

    removed local image files in addition module

--- src/pages/ModTwo.js
+++ src/pages/ModTwo.js
@@ -2,63 +2,60 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import "../styles.css";
 import HTMLFlipBook from "react-pageflip";
-import titleBackground from "../components/images/apOne1.png";
-import pageOne from "../components/images/apTwo1.png";
-import pageTwo from "../components/images/apThree1.png";
-import pageThree from "../components/images/apFour1.png";
-import pageFour from "../components/images/apFive1.png";
-import pageFive from "../components/images/apSix1.png";
-import pageSix from "../components/images/mpSeven1.png";
-import pageSeven from "../components/images/mpEight1.png";
+import pageSeven from "../components/images/mpEight2.png";
 import ReactPlayer from 'react-player';
 
 function ModTwo(props) {
   return (
     <div className="myBook">
       <Navigation />
-        <HTMLFlipBook width={400} height={400} style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}>
+      <HTMLFlipBook 
+          width={500} 
+          height={500} 
+          style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}
+        >
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${titleBackground})`, width: "400px", height: "400px"}}>Page 1</div>
+              <img src="https://photos.smugmug.com/photos/i-hK42F9p/0/c096347c/X2/i-hK42F9p-X2.png" alt="Page One" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageOne})`, width: "400px", height: "400px"}}>Page 2</div>
+              <img src="https://photos.smugmug.com/photos/i-JwZWzHt/0/b4813e60/X2/i-JwZWzHt-X2.png"  alt="Page Two" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwo})`, width: "400px", height: "400px"}}>Page 3</div>
+              <img src="https://photos.smugmug.com/photos/i-ttLfBwz/0/c96113fe/X2/i-ttLfBwz-X2.png"  alt="Page Three" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageThree})`, width: "400px", height: "400px"}}>Page 4</div>
+              <img src="https://photos.smugmug.com/photos/i-LRX7VKM/0/3a012687/X2/i-LRX7VKM-X2.png"  alt="Page Four" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageFour})`, width: "400px", height: "400px"}}>Page 5</div>
+              <img src="https://photos.smugmug.com/photos/i-tJHdfdr/0/de4a4088/X2/i-tJHdfdr-X2.png" alt="Page Five" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageFive})`, width: "400px", height: "400px"}}>Page 6</div>
+              <img src="https://photos.smugmug.com/photos/i-mKs9NPK/0/b232fb1e/X2/i-mKs9NPK-X2.png" alt="Page Six" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageSix})`, width: "400px", height: "400px"}}>Page 7</div>
+              <img src="https://photos.smugmug.com/photos/i-3Thwkgr/0/M/i-3Thwkgr-M.png" alt="Page Seven" style={{width: "550px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="container">
-              <div className="video col-sm-6">
-                <div className="page-image" style={{backgroundImage: `url(${pageSeven})`, width: "400px", height: "400px"}}> 
-                    <div className="row" style={{height: "300px"}}>                       
+              <div className="video">
+                <div className="page-image" style={{backgroundImage: `url(${pageSeven})`, width: "500px", height: "500px"}}> 
+                    <div className="row">                       
                       <ReactPlayer 
-                          style={{postion: "relative", marginTop: "5%", marginLeft: "1vw"}}
+                          style={{position: "relative", marginTop: "16%", marginLeft: "1vw"}}
                           url="https://youtu.be/NhWrKZd4Gck" 
                       />
                     </div>
@@ -72,3 +69,4 @@ function ModTwo(props) {
 }
 
 export default ModTwo;
+
