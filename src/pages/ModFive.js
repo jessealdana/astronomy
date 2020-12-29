32e0aaaddf7cd5b28e71d4c88914011dc6bef22f
Jesse Aldana <jesse.aldana@gmail.com>
2020-12-28 19:27:37 -0800

    removed local image files for module five and added image addresses

--- src/pages/ModFive.js
+++ src/pages/ModFive.js
@@ -2,34 +2,34 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import "../styles.css";
 import HTMLFlipBook from "react-pageflip";
-import titleBackground from "../components/images/cpOne1.png";
-import pageOne from "../components/images/cpTwo1.png";
-import pageTwo from "../components/images/cpThree1.png";
-import pageThree from "../components/images/mpEight1.png";
 
 function ModFive(props) {
   return (
     <div className="myBook">
       <Navigation />
-        <HTMLFlipBook width={400} height={400} style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}>
+      <HTMLFlipBook 
+          width={500} 
+          height={500} 
+          style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}
+        >
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${titleBackground})`, width: "400px", height: "400px"}}>Page 1</div>
+              <img src="https://photos.smugmug.com/photos/i-dKDBgJm/0/4e615a0b/X2/i-dKDBgJm-X2.png" alt="Page One" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageOne})`, width: "400px", height: "400px"}}>Page 2</div>
+              <img src="https://photos.smugmug.com/photos/i-S9rWgXQ/0/a6ce3113/X2/i-S9rWgXQ-X2.png"  alt="Page Two" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwo})`, width: "400px", height: "400px"}}>Page 3</div>
+              <img src="https://photos.smugmug.com/photos/i-rgnVXf3/0/589e6a46/X2/i-rgnVXf3-X2.png"  alt="Page Three" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageThree})`, width: "400px", height: "400px"}}>Page 4</div>
+              <img src="https://photos.smugmug.com/photos/i-r6H8XKG/0/336402a5/M/i-r6H8XKG-M.png"  alt="Page Four" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
         </HTMLFlipBook>
