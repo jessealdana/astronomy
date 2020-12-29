2a66c83c683d401f8159e60b51c7e58ec166bbe2
Jesse Aldana <jesse.aldana@gmail.com>
2020-12-29 12:10:34 -0800

    removed local images from module four and added image addresses

--- src/pages/ModFour.js
+++ src/pages/ModFour.js
@@ -2,178 +2,154 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import "../styles.css";
 import HTMLFlipBook from "react-pageflip";
-import titleBackground from "../components/images/2012pOne1.png";
-import pageOne from "../components/images/2012pTwo1.png";
-import pageTwo from "../components/images/2012pThree1.png";
-import pageThree from "../components/images/2012pFour1.png";
-import pageFour from "../components/images/2012pFive1.png";
-import pageFive from "../components/images/2012pSix1.png";
-import pageSix from "../components/images/2012pSeven1.png";
-import pageSeven from "../components/images/2012pEight1.png";
-import pageEight from "../components/images/2012pNine1.png";
-import pageNine from "../components/images/2012pTen1.png";
-import pageTen from "../components/images/2012pEleven1.png";
-import pageEleven from "../components/images/2012pTwelve1.png";
-import pageTwelve from "../components/images/2012pThirteen1.png";
-import pageThirteen from "../components/images/2012pFourteen1.png";
-import pageFourteen from "../components/images/2012pFifteen1.png";
-import pageFifteen from "../components/images/2012pSixteen1.png";
-import pageSixteen from "../components/images/2012pSeventeen1.png";
-import pageSeventeen from "../components/images/2012pEighteen1.png";
-import pageEighteen from "../components/images/2012pNineteen1.png";
-import pageNineteen from "../components/images/2012pTwenty1.png";
-import pageTwenty from "../components/images/2012pTwentyone1.png";
-import pageTwentyOne from "../components/images/2012pTwentytwo1.png";
-import pageTwentyTwo from "../components/images/2012pTwentythree1.png";
-import pageTwentyThree from "../components/images/2012pTwentyfour1.png";
-import pageTwentyFour from "../components/images/2012pTwentyfive1.png";
-import pageTwentyFive from "../components/images/2012pTwentysix1.png";
-import pageTwentySix from "../components/images/2012pTwentyseven1.png";
-import pageTwentySeven from "../components/images/mpEight1.png";
 
 function ModFour(props) {
   return (
     <div className="myBook">
       <Navigation />
-        <HTMLFlipBook width={400} height={400} style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}>
+      <HTMLFlipBook 
+          width={500} 
+          height={500} 
+          style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}
+        >
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${titleBackground})`, width: "400px", height: "400px"}}>Page 1</div>
+              <img src="https://photos.smugmug.com/2012/i-jN9jvjz/0/796fe958/X2/2012p1-X2.png" alt="Page One" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageOne})`, width: "400px", height: "400px"}}>Page 2</div>
+              <img src="https://photos.smugmug.com/2012/i-6ndCnfb/0/0ae7877a/X2/2012p2-X2.png"  alt="Page Two" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwo})`, width: "400px", height: "400px"}}>Page 3</div>
+              <img src="https://photos.smugmug.com/2012/i-x2MbbVK/0/3eca3b10/X2/2012p3-X2.png"  alt="Page Three" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageThree})`, width: "400px", height: "400px"}}>Page 4</div>
+              <img src="https://photos.smugmug.com/2012/i-Q6TJ4BT/0/fa5faf92/X2/2012p4-X2.png"  alt="Page Four" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageFour})`, width: "400px", height: "400px"}}>Page 5</div>
+              <img src="https://photos.smugmug.com/2012/i-gqtPvRk/0/fab8f245/X2/2012p5-X2.png" alt="Page Five" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageFive})`, width: "400px", height: "400px"}}>Page 6</div>
+              <img src="https://photos.smugmug.com/2012/i-bVZbT7K/0/07ce4003/X2/2012p6-X2.png" alt="Page Six" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageSix})`, width: "400px", height: "400px"}}>Page 7</div>
+              <img src="https://photos.smugmug.com/2012/i-7XQ7NGx/0/ac98b19d/X2/2012p7-X2.png" alt="Page Seven" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageSeven})`, width: "400px", height: "400px"}}>Page 8</div>
+              <img src="https://photos.smugmug.com/2012/i-MZnfGQV/0/b3b4bb9c/X2/2012p8-X2.png"  alt="Page Eight" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageEight})`, width: "400px", height: "400px"}}>Page 9</div>
+              <img src="https://photos.smugmug.com/2012/i-JRr3GMq/0/3bc43be4/X2/2012p9-X2.png"  alt="Page Nine" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageNine})`, width: "400px", height: "400px"}}>Page 10</div>
+              <img src="https://photos.smugmug.com/2012/i-pkKXz8v/0/72417432/X2/2012p10-X2.png"  alt="Page Ten" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTen})`, width: "400px", height: "400px"}}>Page 11</div>
+              <img src="https://photos.smugmug.com/2012/i-FNmxHLp/0/9cab9772/X2/2012p11-X2.png" alt="Page Eleven" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageEleven})`, width: "400px", height: "400px"}}>Page 12</div>
+              <img src="https://photos.smugmug.com/2012/i-Wf2RkTg/0/fee6674f/X2/2012p12-X2.png" alt="Page Twelve" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwelve})`, width: "400px", height: "400px"}}>Page 13</div>
+              <img src="https://photos.smugmug.com/2012/i-qsPBdbW/0/e2130317/X2/2012p13-X2.png" alt="Page Thirteen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageThirteen})`, width: "400px", height: "400px"}}>Page 14</div>
+              <img src="https://photos.smugmug.com/2012/i-fqnNKH9/0/4cab800f/X2/2012p14-X2.png"  alt="Page Fourteen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageFourteen})`, width: "400px", height: "400px"}}>Page 15</div>
+              <img src="https://photos.smugmug.com/2012/i-wz37zfH/0/4d79a1cc/X2/2012p15-X2.png"  alt="Page Fifteen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageFifteen})`, width: "400px", height: "400px"}}>Page 16</div>
+              <img src="https://photos.smugmug.com/2012/i-s669mMS/0/8dff1257/X2/2012p16-X2.png"  alt="Page Sixteen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageSixteen})`, width: "400px", height: "400px"}}>Page 17</div>
+              <img src="https://photos.smugmug.com/2012/i-2mfNpgB/0/33affcdc/X2/2012p17-X2.png" alt="Page Seventeen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageSeventeen})`, width: "400px", height: "400px"}}>Page 18</div>
+              <img src="https://photos.smugmug.com/2012/i-kWVhXxB/0/72c4e0a6/X2/2012p18-X2.png" alt="Page Eighteen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageEighteen})`, width: "400px", height: "400px"}}>Page 19</div>
+              <img src="https://photos.smugmug.com/2012/i-wn2B2GZ/0/b3923939/X2/2012p19-X2.png"  alt="Page Nineteen" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageNineteen})`, width: "400px", height: "400px"}}>Page 20</div>
+              <img src="https://photos.smugmug.com/2012/i-MbgGLGV/0/831fcb8d/X2/2012p20-X2.png" alt="Page Twenty" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwenty})`, width: "400px", height: "400px"}}>Page 21</div>
+              <img src="https://photos.smugmug.com/2012/i-DXbFPq9/0/1d56cd4a/X2/2012p21-X2.png" alt="Page Twentyone" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentyOne})`, width: "400px", height: "400px"}}>Page 22</div>
+              <img src="https://photos.smugmug.com/2012/i-cr2MXjt/0/cacb2c9c/X2/2012p22-X2.png" alt="Page Twentytwo" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentyTwo})`, width: "400px", height: "400px"}}>Page 23</div>
+              <img src="https://photos.smugmug.com/2012/i-TLNQsK9/0/e24e4ccd/X2/2012p23-X2.png" alt="Page Twentythree" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentyThree})`, width: "400px", height: "400px"}}>Page 24</div>
+              <img src="https://photos.smugmug.com/2012/i-t36HFdq/0/ae85daad/X2/2012p24-X2.png" alt="Page Twentyfour" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentyFour})`, width: "400px", height: "400px"}}>Page 25</div>
+              <img src="https://photos.smugmug.com/2012/i-gGfTkvs/0/4b0dbc5a/X2/2012p25-X2.png" alt="Page Twentyfive" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentyFive})`, width: "400px", height: "400px"}}>Page 26</div>
+              <img src="https://photos.smugmug.com/2012/i-MkKXbVd/0/fa5672ba/X2/2012p26-X2.png" alt="Page Twentysix" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentySix})`, width: "400px", height: "400px"}}>Page 27</div>
+              <img src="https://photos.smugmug.com/2012/i-sz4kBMs/0/c7c98722/X2/2012p27-X2.png" alt="Page Twentyseven" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
           <div className="demoPage">
             <div className="pageContent">
-              <div className="page-image" style={{backgroundImage: `url(${pageTwentySeven})`, width: "400px", height: "400px"}}>Page 28</div>
+              <img src="https://photos.smugmug.com/photos/i-r6H8XKG/0/336402a5/M/i-r6H8XKG-M.png" alt="Page Twentyeight" style={{width: "500px", height: "500px"}}></img>
             </div>
           </div>
         </HTMLFlipBook>
